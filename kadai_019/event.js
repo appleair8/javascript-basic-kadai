0d43a39cca0a586b919b41656a7e15500c04dcde
appleair8 <127646232+appleair8@users.noreply.github.com>
2023-07-25 18:40:56 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,7 @@
+const btn =document.getElementById('btn');
+const text=document.getElementById('text');
+
+if (btn.addEventListener('click', () =>  {
+   text.textContent = 'ボタンをクリックしました';    
+}));
+
